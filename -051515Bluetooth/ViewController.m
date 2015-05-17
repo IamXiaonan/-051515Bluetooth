50703ce8aafe482682234dcc7b3230b85feec645
MissWan <alinawxn@gmail.com>
2015-05-16 20:02:10 -0400

    discover peripheral
    successfully discovered peripheral.

--- -051515Bluetooth/ViewController.m
+++ -051515Bluetooth/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#define kServiceUUID @"6BC6543C-2398-4E4A-AF28-E4E0BF58D6BC"
+#define kCharacteristicUUID @"9D69C18C-186C-45EA-A7DA-6ED7500E9C97"
 
 @interface ViewController ()
 
@@ -17,11 +19,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.central = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma marks - communicate with Bluetooth device
+//discover bluetooth device
+-(void)discoverDevices{
+    //discover any peripheral devices that are advertising bu calling:
+    //nil for first parameter returns all discovered peripherals. In real app, specify an array of CBUUID object.
+    NSLog(@"scanning...");
+    [self.central scanForPeripheralsWithServices:nil options:nil];
+    
+}
+
+#pragma marks - delegate
+
+-(void)centralManagerDidUpdateState:(CBCentralManager *)central{
+    switch (central.state) {
+        case CBCentralManagerStatePoweredOn:
+            // Scans for any peripheral
+            [self.central scanForPeripheralsWithServices:@[ [CBUUID UUIDWithString:kServiceUUID] ] options:@{CBCentralManagerScanOptionAllowDuplicatesKey : @YES }];
+            break;
+        default:
+            NSLog(@"Central Manager did change state");
+            break;
+    }
+}
+
+-(void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI{
+    NSLog(@"Discovered %@", peripheral.name);
+    [self.central stopScan];
+}
+
 
 @end
